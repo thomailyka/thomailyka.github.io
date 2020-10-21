b41b7988bf2ae767256a8e717c063794f1d4fb92
Thomai Lyka <tlyka@and.digital>
2020-10-21 11:24:07 +0100

    my first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/**
+ Below are the exercises to complete:
+  Exercise 5.1.1 - Link the JavaScript file to each of your HTML files. Test this works by 
+                   adding a console log containing your name, and view in your browser.
+
+  Exercise 5.2.1 - Create a variable with your name as a string, and add a new console log with this variable.
+  Exercise 5.2.2 - Create another variable with your age as a number, and add a new console log with this variable.
+  Exercise 5.2.3 - Create a variable containing the city where you live using const, print it with console log, 
+                   then try to re-assign the value. What happens?
+
+  Exercise 5.3.1 - Create a new object using the information of your first project on your “My Top 5” page, 
+                   and print it using console.log from inside the top-five.js file (Make sure you link it 
+                   into your top-five.html file)
+
+  Exercise 5.4.1 - Create an array of objects including the one we created in Exercise 3, with the remaining 
+                   projects on your “My Top 5” page.
+  
+  Exercise 5.5.1 - Purposefully remove the link key from your first object in the array. Now add a conditional 
+                   that checks for the key in this first object, and handles for this missing data. Make sure 
+                   the code prints a message saying that the data is missing, to keep the objects consistent.
+
+  Exercise 5.6.1 - Create a for loop that cycles through each object in your array, and prints each value to the console. 
+  Exercise 5.6.2 - Move your conditional into your for loop and check on each object, rather than just the first.
+  Exercise 5.6.3 - Experiment with how you print each value to the console. Try adding titles to each value 
+                   and line breaks after each object .
+
+ */
